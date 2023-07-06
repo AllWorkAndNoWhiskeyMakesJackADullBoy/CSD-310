3df5c768dc9b708a4d3e532511b495c65338988a
Jeff Haberman <jhaberman@my365.bellevue.edu>
2023-07-06 07:49:03 -0500

    adding pytech files

---
+++ module_6/assignment 2/pytech_update.py
@@ -0,0 +1,37 @@
+#Assignment 6.2
+#July 5th, 2023
+#Jeff Haberman
+#CYBR 410
+#Python script to find all documents in student collection, then update one record, and return updated document
+
+#installing python tool for MongoDB
+import pymongo
+from pymongo import MongoClient
+
+#connection to MongoAtlas and setting variables for database and collection
+url="mongodb+srv://admin:admin@cluster0.uotjn6t.mongodb.net/"
+client= MongoClient(url)
+db = client.pytech
+students = db.students.find()
+
+#printing all documents in the students collection
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+for student in students:
+ 
+    print(f"Student ID: {student[ 'student_id']}")
+    print(f"First Name: {student['first_name']}")
+    print(f"Last Name: {student['last_name']}\n")
+
+#updating last name of student_id 1007
+
+updated_student = db.students.update_one({"student_id": 1007}, {"$set": {"last_name": "Smith"}})
+
+#using find_one method to find student
+find_student = db.students.find_one({'student_id':1007})
+
+#printing the document for student_id 1007
+print("-- DISPLAYING STUDENT DOCUMENT 1007 --")
+
+print(f"Student ID: {find_student['student_id']}")
+print(f"First Name: {find_student['first_name']}")
+print(f"Last Name: {find_student['last_name']}")
---
+++ module_6/assignment 3/pytech_delete.py
@@ -0,0 +1,80 @@
+#Assignment 6.3
+#July 5th, 2023
+#Jeff Haberman
+#CYBR 410
+#Python script to use find() method to display all documents in a collection, use insert_one method to add a new document, delete_one method to delete a document
+
+#installing python tool for MongoDB
+import pymongo
+from pymongo import MongoClient
+
+#connection to MongoAtlas and setting variables for database and collection
+url="mongodb+srv://admin:admin@cluster0.uotjn6t.mongodb.net/"
+client= MongoClient(url)
+db = client.pytech
+students = db.students.find()
+
+#printing all documents in the students collection
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+for student in students:
+ 
+    print(f"Student ID: {student[
+    'student_id'
+    ]
+    }")
+ 
+    print(f"First Name: {student[
+    'first_name'
+    ]
+    }")
+ 
+    print(f"Last Name: {student[
+    'last_name'
+    ]
+    }\n")
+
+#create document to insert
+new_student_document = {"student_id": 1010, "first_name": "John", "last_name": "Doe"}
+
+#inserting new student into collection
+insert_new_student = db.students.insert_one(new_student_document)
+
+#display inserted document
+print(f"Inserted student record {new_student_document{"first_name"}} {new_student_document{"last_name"}} into the students collection with document id {insert_new_student}")
+
+#using find_one method to find new student
+new_student = db.students.find_one({'student_id':1010})
+
+#printing the document for new student
+print("-- DISPLAYING STUDENT TEST DOC --")
+
+print(f"Student ID: {new_student[
+'student_id'
+]
+}")
+print(f"First Name: {new_student[
+'first_name'
+]
+}")
+print(f"Last Name: {new_student[
+'last_name'
+]
+}\n")
+
+
+#displaying all students 
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+print(f"Student ID: {student[
+'student_id'
+]
+}")
+ 
+print(f"First Name: {student[
+'first_name'
+]
+}")
+ 
+print(f"Last Name: {student[
+'last_name'
+]
+}\n")
