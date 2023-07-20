604e8bfedb7722292375dcb9efff3c76ec3d298e
Jeff Haberman <jhaberman@my365.bellevue.edu>
2023-07-19 19:26:27 -0500

    updated assignments

--- module_8/assignmnet_3/pysports_queries.py
+++ module_8/assignmnet_3/pysports_queries.py
@@ -11,15 +11,47 @@ config = {
     'raise_on_warnings': True
 }
 
-#database connection attempt
 try:
     #connect to the database with variable, add to variable for printing
     db = mysql.connector.connect(**config)
+
+    #set cursor position for query
+    cursor = db.cursor()
+
+    #team table query statement
+    team_query = ("SELECT team_id, team_name, mascot FROM team")
+
+    #player table query statement
+    player_query = ("SELECT player_id, first_name, last_name,team_id FROM player")
+
+    #perform team table query statement
+    cursor.execute(team_query)
 
     #print successful connection to database
     print("\n Database user {} connected to MySQL on host {} with database {} ".format(config["user"], config["host"], config["database"]))
 
     input("\n\n Press any key to continue...")
+    
+    #print out all team records
+    print("\n\n-- DISPLAYING TEAM RECORDS --")
+
+    for (team_id, team_name, mascot) in cursor:
+        print("Team ID: {}".format(team_id))
+        print("Team Name: {}".format(team_name))
+        print("Mascot: {}".format(mascot))
+        print("\n")
+   
+    #perform player table query statement
+    cursor.execute(player_query)
+
+    #print out all player records
+    print("-- DISPLAYING PLAYER RECORDS --")
+    for (player_id, first_name, last_name, team_id) in cursor:
+        print("Player ID: {}".format(player_id))
+        print("First Name: {}".format(first_name))
+        print("Last Name: {}".format(last_name))
+        print("Team ID: {}".format(team_id))
+        print("\n")
 
 #error handling section
 except mysql.connector.Error as err:
@@ -39,4 +71,5 @@ except mysql.connector.Error as err:
 #close the database connection
 finally:
     db.close()
-    
+
+input("\nPress any key to continue...")
